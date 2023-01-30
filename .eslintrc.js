bdfbfd9aa0f20980df27d0ba33e1acff516838eb
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-01-30 20:10:55 +0100

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
     'next/core-web-vitals',
     'prettier',
     'plugin:tailwindcss/recommended',
+    "eslint:recommended", "next"
+}
   ],
   rules: {
     "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
+    "@next/next/no-page-custom-font": "off",
     semi: ['error', 'never'],
     quotes: ['error', 'single'],
     'no-restricted-imports': [
