560c4dd17dc0fbd31bb99b616221249f4f8a4c07
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 19:43:09 +0100

    Update skills.tsx

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -12,7 +12,7 @@ export default function Skills(){
      <div className={styles.grid}>
 	     
 	     <Link href="/"><a className={styles.cardhome}><h2>home</h2></a></Link>
-	    
+	    <br />
             <h2 className={styles.title}>Skills</h2>
             <p className={styles.description}>
              HTML, CSS, SASS, JS, React, Redux, Next.js, Firebase, NPM, Gulp, Git, MySQL, Wordpress.
