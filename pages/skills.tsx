7cc274f87451c7a2ee43e12c8713d605ac29a83a
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 19:16:18 +0100

    update skills

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -7,9 +7,10 @@ import styles from '@/styles/Home.module.css'
 
 export default function Skills(){
     return(
-	<div className={styles.grid}>
-		    <Link href="/"><a className={styles.card}><h2>home</h2></a></Link>
+	<div className={styles.container}>
+		    
      <div className={styles.grid}>
+	     <Link href="/"><a className={styles.card}><h2>home</h2></a></Link>
             <h2 className={styles.title}>Skills</h2>
             <p className={styles.description}>
              HTML, CSS, SASS, JS, React, Redux, Next.js, Firebase, NPM, Gulp, Git, MySQL, Wordpress.
