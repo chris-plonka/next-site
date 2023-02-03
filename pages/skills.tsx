583a96558adb2f8aa71306893b56c7bd90b5bff0
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 19:14:06 +0100

    add class grid

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -7,7 +7,7 @@ import styles from '@/styles/Home.module.css'
 
 export default function Skills(){
     return(
-	<div className={styles.container}>
+	<div className={styles.grid}>
 		    <Link href="/"><a className={styles.card}><h2>home</h2></a></Link>
      <div className={styles.grid}>
             <h2 className={styles.title}>Skills</h2>
