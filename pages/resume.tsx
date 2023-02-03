377b43630a1d09976b80ff139636ed44fa873ad9
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 23:00:41 +0100

    update.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,16 +5,37 @@ import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 
 
-export default function Pesume(){
-    return(
+export default function Resume(){
+    
+	       const viewer = useRef(null);
+
+    useEffect(() => {
+      import('@pdftron/webviewer').then(() => {
+        WebViewer(
+          {
+            path: '/webviewer/lib',
+            initialDoc: '/files/pdftron_about.pdf',
+          },
+          viewer.current,
+        ).then((instance) => {
+            const { docViewer } = instance;
+            // you can now call WebViewer APIs here...
+          });
+      })
+    }, []);
+
+
+    return (
+      return(
 	<div className={styles.container}>
 		  <Link href="/"><a className={styles.cardhome}><h2>home</h2></a></Link>
-     <div className={styles.grid}>
-            <h2 className={styles.title}>Resume</h2>
-            <p className={styles.description}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-        </div>
-		</div>
+     <div className={styles.grid}>    
+      <div className="MyComponent">
+        <div className="header">React sample</div>
+        <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
+      </div>
+		    </div>
+		    </div>
+    );
     )
 }
