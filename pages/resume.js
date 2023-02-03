e2a3af463b4dcda8285f429295861473bdf2dcaf
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 21:59:20 +0100

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -1,36 +1,30 @@
-import React from 'react';
+import {useEffect, useRef} from 'react';
 
-import Article from '../components/Article';
-import PDFLayout from '../components/PDFLayout';
-import pdfHelper from '../lib/pdfHelper';
+export default function HomePage() {
 
-class IndexPage extends React.Component {
-  static async getInitialProps({ req, res, query }) {
-    const exportPDF = query.exportPDF === 'true';
-    const isServer = !!req;
+    const viewer = useRef(null);
 
-    if (isServer && exportPDF) {
-      const buffer = await pdfHelper.componentToPDFBuffer(
-        <PDFLayout><Article/></PDFLayout>
-      );
+    useEffect(() => {
+      import('@pdftron/webviewer').then(() => {
+        WebViewer(
+          {
+            path: '/webviewer/lib',
+            initialDoc: '/files/pdftron_about.pdf',
+          },
+          viewer.current,
+        ).then((instance) => {
+            const { docViewer } = instance;
+            // you can now call WebViewer APIs here...
+          });
+      })
+    }, []);
 
-      // with this header, your browser will prompt you to download the file
-      // without this header, your browse will open the pdf directly
-      res.setHeader('Content-disposition', 'attachment; filename="resume.pdf');
-      
-      // set content type
-      res.setHeader('Content-Type', 'application/pdf');
 
-      // output the pdf buffer. once res.end is triggered, it won't trigger the render method
-      res.end(buffer);
-    }
-
-    return {};
-  }
-
-  render() {
-    return (<Article/>)
-  }
+    return (
+      <div className="MyComponent">
+        <div className="header">React sample</div>
+        <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
+      </div>
+    );
+  
 }
-
-export default IndexPage;
