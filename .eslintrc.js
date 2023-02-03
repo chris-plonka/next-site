1cbd081454b095ec401402d9c2a3a48bd166bd96
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-03 22:33:14 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     'prettier',
     'plugin:tailwindcss/recommended',
     "eslint:recommended", "next"
-}
+
   ],
   rules: {
     "react/no-unescaped-entities": "off",
@@ -18,5 +18,5 @@ module.exports = {
         patterns: ['../'],
       },
     ],
-  },
+  }
 }
