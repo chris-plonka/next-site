c14cf0dd6409475f7e3cc865f44e8ae39b2ed94c
chris-plonka <57814080+chris-plonka@users.noreply.github.com>
2023-02-04 00:48:30 +0100

    update resume.tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -6,25 +6,22 @@ import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 
 
-<<<<<<< HEAD
 export default function Resume() {
     
 	 const viewer = useRef(null);
-=======
+
 export default function Resume(){
     
 	       const viewer = useRef(null);
->>>>>>> 2c3400167a245eb1b99d45bec151c27cd6dffea7
+
 
     useEffect(() => {
       import('@pdftron/webviewer').then(() => {
         WebViewer(
           {
-<<<<<<< HEAD
-            path: 'webviewer/lib',
-=======
+
             path: '/webviewer/lib',
->>>>>>> 2c3400167a245eb1b99d45bec151c27cd6dffea7
+
             initialDoc: '/files/resume.pdf',
           },
           viewer.current,
@@ -35,25 +32,9 @@ export default function Resume(){
       })
     }, []);
 
-<<<<<<< HEAD
+
 	
-	return (
-            
-			<div className="MyComponent">
-		<div className={styles.containerhome}>	
-        <Link href="/"><a className={styles.cardhome}><h2>Home</h2></a></Link>
-		</div>
-        <div className="webviewer" ref={viewer} style={{height: "90vh"}}></div>
 
-		 <div className="py-2 px-400">
-		  
-		</div>
-
-
-        </div>
-=======
-
-    return (
       return(
 	<div className={styles.container}>
 		  <Link href="/"><a className={styles.cardhome}><h2>home</h2></a></Link>
@@ -65,6 +46,5 @@ export default function Resume(){
 		    </div>
 		    </div>
     );
->>>>>>> 2c3400167a245eb1b99d45bec151c27cd6dffea7
     )
 }
